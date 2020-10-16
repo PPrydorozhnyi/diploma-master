2aaf147fa7c83b4d046fa20962596841e7935e64
PPrydorozhnyi <Peter35128@gmail.com>
2020-10-17 02:27:36 +0300

    init

---
+++ src/test/java/com/diploma/master/DiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diploma.master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
